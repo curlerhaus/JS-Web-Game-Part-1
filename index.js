c388d7eebd9c1bfa2e9797a2c7392d68965db05e
curlerhaus <101598928+curlerhaus@users.noreply.github.com>
2022-06-06 13:30:04 -0500

    newItem final

--- index.js
+++ index.js
@@ -15,3 +15,14 @@ newImage('assets/crate.png', 150, 200)
 newImage('assets/well.png', 500, 425)
 newImage('assets/pine-tree.png', 450, 200)
 
+function newItem(pic, left, bottom){
+    let thing = newImage(pic, left, bottom)
+
+    thing.addEventListener('dblclick', function(){
+       thing.remove()
+    })
+}
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/sheild.png', 165, 185)
+newItem('assets/staff.png', 600, 100)

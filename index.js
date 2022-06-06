58a4c6ffc5de6282cef49cc6b61e454c4451ccce
curlerhaus <101598928+curlerhaus@users.noreply.github.com>
2022-06-06 13:29:18 -0500

    newImages

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(pic, left, bottom){
+    let greenCharacter = document.createElement('img')
+    greenCharacter.src = pic
+    greenCharacter.style.position = 'fixed'
+    greenCharacter.style.left = left + 'px'
+    greenCharacter.style.bottom = bottom + 'px'
+    document.body.append(greenCharacter)
+    return greenCharacter
+}
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+newImage('assets/pine-tree.png', 450, 200)
+
